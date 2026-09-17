7346873e3a4a99843ee54131d4de22f94edced12
Caio (Desktop) <caioa.morais@gmail.com>
2026-09-16 22:32:16 -0300

    feat: init module market-search^

---
+++ app/src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MarketSearchModule } from './market-search/market-search.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MarketSearchModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
